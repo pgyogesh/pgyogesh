94bf090349419bc0d1a03bd4e90a3b49eb384150
pgyogesh <pgyogesh@outlook.com>
2022-05-30 16:02:50 +0530

    updated

--- bookmarks/obsedian.js
+++ bookmarks/obsedian.js
@@ -1,3 +1,5 @@
+// When clicked on this bookmark, it will opene the obsedian node for the ticket for the zendesk url. If tab is not zendesk,
+// it will prompt to enter the ticket number.
 javascript:if (window.location.href.indexOf('yugabyte.zendesk.com') === -1) {
     var ticket = prompt('Please enter your ticket number');
     if (ticket) {
