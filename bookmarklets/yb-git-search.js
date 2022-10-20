27bdbbb4144e18f644cdff97b6b02777095fbe88
Yogesh Jadhav <pgyogesh@outlook.com>
2022-10-20 09:40:55 +0530

    Updated  git search bookmarklet

--- bookmarklets/yb-git-search.js
+++ bookmarklets/yb-git-search.js
@@ -2,5 +2,5 @@
 javascript: var pattern = prompt("What you want me to search for?");
 if (pattern) {
     url = "https://github.com/yugabyte/yugabyte-db/search?q=" + pattern; 
-    window.location.href = url;
+    window.open(url, '_blank');
 }
